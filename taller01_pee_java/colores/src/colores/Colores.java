89e07a71a78a2b7a18eb83d5c3129df373917622
JuanmaM-G <kporta715@gmail.com>
2025-07-19 23:13:22 -0500

    trabajoextra.1

--- taller01_pee_java/colores/src/colores/Colores.java
+++ taller01_pee_java/colores/src/colores/Colores.java
@@ -11,19 +11,30 @@ public class Colores {
         
         int col1, col2;
         
-        System.out.println("Escoja que combinacion decolores desea hacer");
+        System.out.println("Escoja que combinacion de colores desea hacer");
         
         System.out.println("(1)amarillo");
         System.out.println("(2)azul");
         System.out.println("(3)rojo");
-        
+        // 1+2 = verde | 1+3 = naranja | 2+3 = morado
+        System.out.println("digite el valor del primer color que desea ingresar");
         col1 = scan.nextInt();
+        System.out.println("digite el valor del segundo color que desea ingresar");
         col2 = scan.nextInt();
         
-        if (col1 <=3  || col2>0 || col1 <=3 || col2>0)
-            System.out.println("los datos ingresados no son validos");
-        
-        
+        if (col1==1 && col2 == 2 || col1==2 && col2 == 1){
+         System.out.println("la combinacion de tus colores es VERDE");
+        }
+        else if (col1 ==1 && col2 == 3 || col1 ==3 && col2 == 1){
+            System.out.println("la combinacion de tus colores es NARANJA");
+        }else if (col1 ==2 && col2 ==3 || col1 ==3 && col2 ==2){
+            System.out.println("la combinacion de tus colores es MORADO");
+        }else if (col1==1 && col2==1 || col1==2 && col2==2 || col1==3 && col2==3){
+            System.out.println("No es valido ingresar el mismo digito para ambos valores, Prueba con digitos diferentes ");
+        }else if (col1 >3 || col2>3 || col1 <1 || col2<1) {
+            System.out.println("los digitos que ingresaste no son validos");
+        }
+   
     }
     
 }
